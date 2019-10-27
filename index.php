550094d272c0edc61ce09901a234cdc801268cdf
ristep <risepan@gmail.com>
2019-10-27 20:03:57 +0100

    Test

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// Headers not tested quite well
 header("Content-Type: application/json");
 header('Access-Control-Allow-Origin: *');
 //header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
