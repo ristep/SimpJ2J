753e6a7fde268100b9c0de7cadfa5ad6e7a1d141
ristep <risepan@gmail.com>
2019-10-28 11:27:25 +0100

    Some small corections

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Simple JSON to SQL to JSON php api
-Single entry point PHP api, JSON recuest and respond
+Single entry point PHP api, JSON request and respond
--- functions.php
+++ functions.php
@@ -1,82 +1,92 @@
 <?php
 
-// $flt_dlmt = "~";
-// $flt_dlmt = "'";
-// $flt_dlmt = "|";
-$flt_dlmt = "`";
 $default_page = 5;
 
-function parse_filter( $fltstr ){  
-	$arr3 = explode (',',$fltstr);
+function parse_filter( $fltStr ){  
+	$arr3 = explode (',',$fltStr);
 	//print_r( $arr3);
   $not = '';
   $flt = '';
   switch ($arr3[1]) {
-      // "sw”: start with (string starts with value)
+		
+		// "sw”: start with (string starts with value)
     case 'nsw':
       $not = 'not';
     case 'sw':
       $flt .= $arr3[0] . " $not like '" . $arr3[2] . "%' ";
       break;
-      // “cs”: contain string (string contains value)
+		
+		// “cs”: contain string (string contains value)
     case 'ncs':
       $not = 'not';
     case 'cs':
       $flt .= $arr3[0] . " $not like '%" . $arr3[2] . "%' ";
       break;
-      // “ew”: end with (string end with value)
+		
+		// “ew”: end with (string end with value)
     case 'new':
       $not = 'not';
     case 'ew':
       $flt .= $arr3[0] . " $not like '%" . $arr3[2] . "' ";
       break;
-      // “eq”: equal (string or number matches exactly)
+		
+		// “eq”: equal (string or number matches exactly)
     case 'neq':
       $not = '!';
     case 'eq':
       $flt .= $arr3[0] . " $not = '" . $arr3[2] . "'";
       break;
-      // “lt”: lower than (number is lower than value)
+		
+		// “lt”: lower than (number is lower than value)
     case 'lt':
       $flt .= $arr3[0] . " < '" . $arr3[2] . "'";
       break;
-      // “le”: lower or equal (number is lower than or equal to value)
+		
+		// “le”: lower or equal (number is lower than or equal to value)
     case 'le':
       $flt .= $arr3[0] . " <= '" . $arr3[2] . "'";
       break;
-      // "ge": greater or equal (number is higher than or equal to value)
+		
+		// "ge": greater or equal (number is higher than or equal to value)
     case 'ge':
       $flt .= $arr3[0] . " >= '" . $arr3[2] . "'";
       break;
-      // "gt": greater than (number is higher than value)
+ 
+		// "gt": greater than (number is higher than value)
     case 'gt':
       $flt .= $arr3[0] . " > '" . $arr3[2] . "'";
       break;
-      // “bt”: between (number is between two comma separated values)
-    case 'nbt':
+	
+		// “bt”: between (number is between two comma separated values)
+		case 'nbt':
       $not = 'not';
     case 'bt':
       $flt .= $not . '(' . $arr3[0] . " > '" . $arr3[2] . "' and " . $arr3[0] . " < '" . $arr3[3] . "' )";
-      break;
-    case 'nbte':
+			break;
+
+		// “nbte”: not between or equal (number is between two comma separated values)
+	  case 'nbte':
       $not = 'not';
     case 'bte':
       $flt .= $not . '(' . $arr3[0] . " >= '" . $arr3[2] . "' and " . $arr3[0] . " <= '" . $arr3[3] . "' )";
       break;
-      // "in" in list number or string is in list separaated by comas
-    case 'nin':
+		
+		// "in" in list number or string is in list separated by comas 
+		case 'nin':
       $not = 'not';
     case 'in':
       $lst = implode(",", array_slice($arr3, 2));
       $flt .= $arr3[0] . " $not in ($lst)";
-      break;
-      // “is”: is null (field contains “NULL” value)
+			break;
+			
+    // “is”: is null (field contains “NULL” value)
     case 'nis':
       $not = 'not';
     case 'is':
       $flt .= $arr3[0] . " is $not NULL ";
       break;
-  }
+	}
+	
   return $flt;
 }
 
@@ -95,27 +105,27 @@ function build_sql($input){
 	$where = "";
 	switch ($input->sqlStatement) {
 	case 'sql':
-		$badwords = ['delete', 'drop', 'insert', 'use', 'grant', 'user', 'update'];
+		$nasty_words = ['delete', 'drop', 'insert', 'use', 'grant', 'user', 'update'];
 		$sql = $input->sql;
-		foreach ($badwords as $wrd) {
+		foreach ($nasty_words as $wrd) {
 			if (stripos($sql, $wrd) !== false)
 				die(json_encode(["OK" => false, "error" => " Bad Robot!!! "]));
 		}
 	break;
 	case 'select': 
-			$fieldarr = array();
-			$wherearr = array();
+			$fieldArr = array();
+			$whereArr = array();
 			if (isset($input->fields))
 				$fields = $input->fields;
 			else
 				$fields = ["*"];
 			foreach ($fields as $fl)
-				array_push($fieldarr, $fl);
+				array_push($fieldArr, $fl);
 
 			if (isset($input->keyData)) {
 				foreach ($input->keyData as $key => $val)
-					array_push($wherearr, "$key='$val'");
-				$where = "WHERE " . implode(' and ', $wherearr);
+					array_push($whereArr, "$key='$val'");
+				$where = "WHERE " . implode(' and ', $whereArr);
 			} elseif (isset($input->where))
 				$where = "WHERE $input->where";
 			elseif (isset($input->filter)){
@@ -151,21 +161,21 @@ function build_sql($input){
 					$order = "ORDER BY " . $input->order;
 			}
 
-			$sql = "SELECT " . implode(',', $fieldarr) . " FROM $input->table $where $order $limit;";
+			$sql = "SELECT " . implode(',', $fieldArr) . " FROM $input->table $where $order $limit;";
 		break;
 		case 'update':
-			$wherearr = array();
+			$whereArr = array();
 			if (isset($input->keyData)) {
 				foreach ($input->keyData as $key => $val)
-					array_push($wherearr, "$key='$val'");
+					array_push($whereArr, "$key='$val'");
 			}	
-			$where = implode(' and ', $wherearr);
+			$where = implode(' and ', $whereArr);
 
-			$fieldarr = array();
+			$fieldArr = array();
 			foreach ($input->data as $pl => $vl)
-				array_push($fieldarr, "$pl=:$pl");
+				array_push($fieldArr, "$pl=:$pl");
 
-				$sql = "UPDATE $input->table SET " . implode(',', $fieldarr) . " WHERE $where ";
+				$sql = "UPDATE $input->table SET " . implode(',', $fieldArr) . " WHERE $where ";
 		break;
 		case 'insert':
 			$nms = array();
@@ -177,12 +187,12 @@ function build_sql($input){
 			$sql = "INSERT INTO `$input->table`(" . implode(',', $nms) . ") VALUES(" . implode(',', $vls) . ");";
 		break;
 		case 'delete':
-			$wherearr = array();
+			$whereArr = array();
 			if (isset($input->keyData)) {
 				foreach ($input->keyData as $key => $val)
-					array_push($wherearr, "$key='$val'");
+					array_push($whereArr, "$key='$val'");
 			}	
-			$where = implode(' and ', $wherearr);
+			$where = implode(' and ', $whereArr);
 
 			$sql = "DELETE FROM `$input->table` WHERE $where;";
 			//file_put_contents('inputDump.txt', $sql, FILE_APPEND);
@@ -195,18 +205,3 @@ function build_sql($input){
 	};
 	return $sql;
 };
-
-// function build_filter($fstr)
-// {
-//   if (strpos($fstr,  $flt_dlmt) === false)
-//     $ret = parse_filter($fstr);
-//   else {
-//     preg_match_all("/\\" .  $flt_dlmt . "(.*?)\\" .  $flt_dlmt . "/", $fstr, $matches);
-//     // print_r($matches);
-//     foreach ($matches[1] as $key => $value) {
-//       $matches[1][$key] = " " . parse_filter($value) . " ";
-//     }
-//     $ret = str_replace($matches[0], $matches[1], $fstr);
-//   }
-//   return $ret;
-// }
